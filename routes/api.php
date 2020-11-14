e490e19867ac5671b918d60c5c3296fa71e99cdc
johntzulik1 <john.tzulik@gmail.com>
2020-11-14 10:20:33 -0600

    cambio de ruta

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('articles', 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
+Route::get('article/{id}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
 Route::put('articles/{id}', 'ArticleController@update');
 Route::delete('articles/{id}', 'ArticleController@delete');
